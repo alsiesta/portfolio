7474415c23aecc38d2039fe05f4b4fdabb068f10
alsiesta <as@daslizenzatelier.de>
2023-04-04 14:10:32 +0200

    added aboutme component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,18 @@ import {MatMenuModule} from '@angular/material/menu';
 import { HeaderComponent } from './header/header.component';
 import { GreetingsectionComponent } from './routing/greetingsection/greetingsection.component';
 import { MainpaneComponent } from './mainpane/mainpane.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { AboutmeComponent } from './routing/aboutme/aboutme.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, HeaderComponent,GreetingsectionComponent, MainpaneComponent],
-  imports: [BrowserModule, AppRoutingModule, MatToolbarModule, MatIconModule, MatButtonModule,MatMenuModule,],
+  declarations: [AppComponent, FooterComponent, HeaderComponent,GreetingsectionComponent, MainpaneComponent, AboutmeComponent, ],
+  imports: [BrowserModule, AppRoutingModule, MatToolbarModule, MatIconModule, MatButtonModule,MatMenuModule, LayoutModule, MatSidenavModule, MatListModule,],
   providers: [],
   bootstrap: [AppComponent],
+  exports: [
+    AboutmeComponent
+  ],
 })
 export class AppModule {}
