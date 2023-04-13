6e407e9487ba823efa806a239cc0a76e49a42d48
alsiesta <as@daslizenzatelier.de>
2023-04-13 14:46:38 +0200

    material button

--- src/app/routing/greetingsection/greetingsection.component.ts
+++ src/app/routing/greetingsection/greetingsection.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
 
 
 @Component({
   selector: 'app-greetingsection',
   templateUrl: './greetingsection.component.html',
-  styleUrls: ['./greetingsection.component.scss']
+  styleUrls: ['./greetingsection.component.scss'],
 })
 export class GreetingsectionComponent {
 
