53509097f827763ebc97052687093d95502d562c
alsiesta <as@daslizenzatelier.de>
2023-04-25 15:33:16 +0200

    separated Material Modules.ts AND added custom text-field-notched outline colors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,12 @@
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './material/material.module';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatCardModule } from '@angular/material/card';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatListModule } from '@angular/material/list';
-import { MatInputModule } from '@angular/material/input';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
 
 import { HeaderComponent } from './header/header.component';
 import { GreetingsectionComponent } from './routing/greetingsection/greetingsection.component';
@@ -29,6 +21,8 @@ import { HeadlineComponent } from './shared/headline/headline.component';
 import { PortfolioCaseComponent } from './shared/portfolio-case/portfolio-case.component';
 import { PortfolioComponent } from './routing/portfolio/portfolio.component';
 import { ContactComponent } from './routing/contact/contact.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -48,20 +42,12 @@ import { ContactComponent } from './routing/contact/contact.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatButtonModule,
-    MatMenuModule,
-    MatCardModule,
-    MatInputModule,
+    MaterialModule,
     LayoutModule,
-    MatSidenavModule,
-    MatListModule,
     BrowserAnimationsModule,
-    FlexLayoutModule,
+    FormsModule
   ],
-  providers: [{provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}],
+  providers: [],
   bootstrap: [AppComponent],
   exports: [AboutmeComponent],
 })
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
+
+const MaterialComponents = [
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatButtonModule,
+  MatMenuModule,
+  MatCardModule,
+  MatInputModule,
+  MatSidenavModule,
+  MatListModule,
+  FlexLayoutModule,]
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents],
+  providers: [{provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}],
+})
+export class MaterialModule { }
