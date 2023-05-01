2149e70fdfb17759b24f73cd77781b42203a7a35
alsiesta <as@daslizenzatelier.de>
2023-05-01 17:02:43 +0200

    added routerLinkActive in both navis

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ImprintComponent } from './routing/imprint/imprint.component';
+import { MainpaneComponent } from './mainpane/mainpane.component';
+import { AboutmeComponent } from './routing/aboutme/aboutme.component';
+import { MyskillsComponent } from './routing/myskills/myskills.component';
+import { PortfolioComponent } from './routing/portfolio/portfolio.component';
+import { ContactComponent } from './routing/contact/contact.component';
 
 const routes: Routes = [
-
+  { path: 'home', component: MainpaneComponent },
+  { path: 'aboutme', component: AboutmeComponent },
+  { path: 'myskills', component: MyskillsComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'contact', component: ContactComponent },
   {path: 'imprint', component: ImprintComponent},
 ];
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,22 +6,24 @@ import { AfterViewInit, Component, EventEmitter, Input, Output } from '@angular/
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements AfterViewInit {
-  ngAfterViewInit(): void {
-    console.log('sidenaviopen: ',this.isSideNaviOpen);
-  }
   sideNavOpenIcon = "./assets/svg/close.svg"
   sideNavClosedIcon = "./assets/svg/burger.svg"
   
-  
+  //nur um zu testen, welchen status die Variable nach dem Load der Seite hat
+  ngAfterViewInit(): void {
+    console.log('sidenaviopen: ',this.isSideNaviOpen);
+  }
 
   @Output() naviClick: EventEmitter<boolean> = new EventEmitter<boolean>();
   @Input() isSideNaviOpen: boolean | undefined;
+  
 
 
   toggleMenu() {
     console.log('toggle was clicked Header and SideNaviOpen status is: ',this.isSideNaviOpen);    
     this.naviClick.emit(this.isSideNaviOpen);
-    this.isSideNaviOpen = !this.isSideNaviOpen
+
+    // this.isSideNaviOpen = !this.isSideNaviOpen
   }
 
   toHome() {
